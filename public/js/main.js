e4ca0011c954f20179d18579924425cdde72754a
Rufus-Sam <68532159+Rufus-Sam@users.noreply.github.com>
2021-10-14 11:57:02 +0530

    creating custom store and maintaining state

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+import * as store from './store.js'
 const socket = io("/")
 
 //this part is the client calling the server,the part in app.js is the server 
@@ -5,4 +6,5 @@ const socket = io("/")
 socket.on('connect', () => {
     console.log('successfully connected to wss/socket.io server')
     console.log(socket.id)
+    store.setSocketId(socket.id);
 })
