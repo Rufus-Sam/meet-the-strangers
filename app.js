a63d4a10752313f9beb44017a49e1bece1d00090
Rufus-Sam <68532159+Rufus-Sam@users.noreply.github.com>
2021-10-14 11:23:40 +0530

    caller id storing in array

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const dotenv = require('dotenv')
+dotenv.config()
+const http = require('http')
+const PORT = process.env.PORT || 3000
+const app = express()
+const server = http.createServer(app)
+const io = require('socket.io')(server)
+
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+let connectedPeers = []
+
+//io.on() is an event listener on server, socket.on() is event listener on client 
+io.on('connection', (socket) => {
+    connectedPeers.push(socket.id);
+    console.log(connectedPeers)
+
+    socket.on('disconnect', () => {
+        console.log("user disconnected")
+
+        const newConnectedPeers = connectedPeers.filter((x) => x !== socket.id)
+        connectedPeers = newConnectedPeers
+        console.log(connectedPeers)
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
+
---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+const socket = io("/")
+
+//this part is the client calling the server,the part in app.js is the server 
+// receiving the call
+socket.on('connect', () => {
+    console.log('successfully connected to wss/socket.io server')
+    console.log(socket.id)
+})
