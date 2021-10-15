5df01b58340f6271910234151685256899813f95
Rufus-Sam <68532159+Rufus-Sam@users.noreply.github.com>
2021-10-15 12:09:01 +0530

    if we get tracks add them to remote stream,also get our tracks from local stream and add it to the connection if it is video call

--- public/js/webRtcHandler.js
+++ public/js/webRtcHandler.js
@@ -4,11 +4,20 @@ import * as ui from './ui.js'
 import * as store from './store.js'
 
 
-let connectedUserDetails;
+let connectedUserDetails
+let peerConnection
 const defaultConstraints = {
     audio: true,
     video: true,
-};
+}
+
+const configuration = {
+    iceServers: [
+        {
+            urls: "stun:stun.l.google.com:13902",
+        },
+    ],
+}
 
 export const getLocalPreview = () => {
     navigator.mediaDevices
@@ -21,6 +30,42 @@ export const getLocalPreview = () => {
             console.log("error occured when trying to get an access to camera");
             console.log(err);
         });
+};
+
+const createPeerConnection = () => {
+    peerConnection = new RTCPeerConnection(configuration);
+
+    peerConnection.onicecandidate = (event) => {
+        console.log("getting ice candidates from stun server");
+        if (event.candidate) {
+            // send our ice candidates to other peer
+        }
+    };
+
+    peerConnection.onconnectionstatechange = (event) => {
+        if (peerConnection.connectionState === "connected") {
+            console.log("succesfully connected with other peer");
+        }
+    };
+
+    // receiving tracks
+    const remoteStream = new MediaStream();
+    store.setRemoteStream(remoteStream);
+    ui.updateRemoteVideo(remoteStream);
+
+    peerConnection.ontrack = (event) => {
+        remoteStream.addTrack(event.track);
+    };
+
+    // add our stream to peer connection
+
+    if (connectedUserDetails.callType === constants.callType.VIDEO_PERSONAL_CODE) {
+        const localStream = store.getState().localStream;
+
+        for (const track of localStream.getTracks()) {
+            peerConnection.addTrack(track, localStream);
+        }
+    }
 };
 export const sendPreOffer = (callType, calleePersonalCode) => {
     connectedUserDetails = {
