8782ad9f3a247ce20c7e7aa0dc9f588631999807
Rufus-Sam <68532159+Rufus-Sam@users.noreply.github.com>
2021-10-18 09:43:46 +0530

    video call ,mic,camera,screen sharing all are working

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@ import * as store from './store.js'
 import * as wss from './wss.js'
 import * as webRtcHandler from './webRtcHandler.js'
 import * as constants from './constants.js'
+import * as ui from './ui.js'
 
 //initialization of socketIO connection
 const socket = io("/")
@@ -32,4 +33,28 @@ personalCodeVideoButton.addEventListener('click', () => {
     const callType = constants.callType.VIDEO_PERSONAL_CODE
     const calleePersonalCode = document.getElementById('personal_code_input').value
     webRtcHandler.sendPreOffer(callType, calleePersonalCode)
+})
+
+//event listeners for video call buttons
+
+const micButton = document.getElementById('mic_button')
+micButton.addEventListener('click', () => {
+    const localStream = store.getState().localStream
+    const micEnabled = localStream.getAudioTracks()[0].enabled
+    localStream.getAudioTracks()[0].enabled = !micEnabled
+    ui.updateMicButton(micEnabled)
+})
+
+const cameraButton = document.getElementById('camera_button')
+cameraButton.addEventListener('click', () => {
+    const localStream = store.getState().localStream
+    const cameraEnabled = localStream.getVideoTracks()[0].enabled
+    localStream.getVideoTracks()[0].enabled = !cameraEnabled
+    ui.updateCameraButton(cameraEnabled)
+})
+
+const switchForScreenSharingButton = document.getElementById('screen_sharing_button')
+switchForScreenSharingButton.addEventListener('click', () => {
+    const screenSharingActive = store.getState().screenSharingActive
+    webRtcHandler.switchBetweenCameraAndScreenSharing(screenSharingActive)
 })
